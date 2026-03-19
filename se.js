6647fb8090451ee36bcf6dc7aa1a3f1ead78987a
spadagabriele19-ux <spadagabriele19@gmail.com>
2026-03-19 20:57:07 +0100

    Create se.js

---
+++ se.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('suv-f-v1').then((cache) => cache.addAll(['index.html', 'manifest.json'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
+
